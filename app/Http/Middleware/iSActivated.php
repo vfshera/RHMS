616a5325ca39fcbda2d7fc2b0d4d3b668b1e8125
FranklinShera <fshera96@gmail.com>
2020-11-05 15:52:14 +0300

    isActivated Middleware

---
+++ app/Http/Middleware/iSActivated.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class iSActivated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if($request->user()->status == '0'){
+
+            toast('Your Account is Not Activated!','error')->position('top')->autoClose(4500);
+
+            return redirect('/profile');
+        }
+
+        return $next($request);
+    }
+}
